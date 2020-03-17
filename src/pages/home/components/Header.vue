1042fdac547978b569e500f4bf81aa8292ee94b8
Jenky1031 <869386961@qq.com>
2020-03-17 14:41:20 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="header">
+		<div class="header-left">
+			<span class="iconfont">&#xe624;</span>
+		</div>
+		<div class="header-input">
+			<span class="iconfont iconsousuo"></span>
+			输入城市/景点/游玩主题
+		</div>
+		<div class="header-right">
+			城市
+			<span class="iconfont">&#xe62d;</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "HomeHeader"
+};
+</script>
+
+<style lang="stylus" scoped>	
+@import '~@styles/variables.styl';
+.header
+	display flex
+	height .86rem
+	line-height .86rem
+	background-color $bgColor
+	color #fff
+	.header-left
+		text-align center
+		width .64rem
+		span
+			font-size .4rem
+	.header-input
+		flex-grow 1
+		background-color #fff
+		border-radius .1rem
+		height .64rem
+		line-height .64rem
+		margin-top .12rem
+		margin-left .2rem
+		padding-left .2rem
+		color #ccc
+	.header-right
+		width 1.24rem
+		text-align center
+		span 	
+			font-size .24rem
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,29 @@
+const path = require("path");
+const resolve = dir => path.join(__dirname, dir);
+const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+
 module.exports = {
-    devServer: {
-        port: 4040, // 端口号
-        host: 'localhost',
-        //https: false, // https:{type:Boolean}
-        //open: true, //配置自动启动浏览器
-        // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
-    }
-}
+	devServer: {
+		port: 4040, // 端口号
+		host: "localhost"
+		//https: false, // https:{type:Boolean}
+		//open: true, //配置自动启动浏览器
+		// proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
+	},
+	chainWebpack: config => {
+		// 添加别名
+		config.resolve.alias
+			.set("vue$", "vue/dist/vue.esm.js")
+			.set("@", resolve("src"))
+			.set("@styles", resolve("src/assets/styles"));
+		//   .set("@assets", resolve("src/assets"))
+		//   .set("@scss", resolve("src/assets/scss"))
+		//   .set("@components", resolve("src/components"))
+		//   .set("@plugins", resolve("src/plugins"))
+		//   .set("@views", resolve("src/views"))
+		//   .set("@router", resolve("src/router"))
+		//   .set("@store", resolve("src/store"))
+		//   .set("@layouts", resolve("src/layouts"))
+		//   .set("@static", resolve("src/static"));
+	}
+};
